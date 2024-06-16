238b8fbee598fd9a6e21d8c5e37bbcc068c35c6b
Suyash Bhawsar <suyashbhawsar@hotmail.com>
2024-06-16 22:22:41 +0530

    Update install_puppet_agent.sh

--- .deployment/install_puppet_agent.sh
+++ .deployment/install_puppet_agent.sh
@@ -4,6 +4,13 @@
 PUPPET_DEB="puppet7-release-jammy.deb"
 PUPPET_URL="https://apt.puppet.com/$PUPPET_DEB"
 PUPPET_PACKAGE="puppet-agent"
+BASHRC_FILE="$HOME/.bashrc"
+PATH_TO_ADD="/opt/puppetlabs/bin"
+
+# Function to check if a path exists in ~/.bashrc
+path_exists_in_bashrc() {
+  grep -qF "$1" "$BASHRC_FILE"
+}
 
 ## Puppet
 # Check if wget is installed, and install it if not
@@ -33,9 +40,12 @@ if [ -f "$PUPPET_DEB" ]; then
 fi
 
 # Add /opt/puppetlabs/bin/ to PATH if not already added
-if [[ ":$PATH:" != *":/opt/puppetlabs/bin:"* ]]; then
-  echo 'export PATH="/opt/puppetlabs/bin:$PATH"' >> ~/.bashrc
+if ! path_exists_in_bashrc "$PATH_TO_ADD"; then
+  echo 'export PATH="/opt/puppetlabs/bin:$PATH"' >> "$BASHRC_FILE"
   export PATH="/opt/puppetlabs/bin:$PATH"
+  echo "Added /opt/puppetlabs/bin/ to \$PATH in $BASHRC_FILE"
+else
+  echo "$PATH_TO_ADD already exists in $BASHRC_FILE. Skipping addition."
 fi
 
 
